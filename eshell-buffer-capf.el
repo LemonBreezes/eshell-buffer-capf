1820fbdd0934fdcb2030093f64fdb61b666745e9
Tony <antonioruiz.math@gmail.com>
2022-01-17 19:03:28 -0500

    Added package metadata.

--- eshell-buffer-capf.el
+++ eshell-buffer-capf.el
@@ -1,5 +1,28 @@
 ;;; packages/eshell-buffer-capf/eshell-buffer-capf.el -*- lexical-binding: t; -*-
 
+;; Author: Antonio Ruiz <antonioruiz.math@gmail.com>
+;; URL: https://github.com/LemonBreezes/eshell-buffer-capf
+;; Version: 1.0
+;; Package-Requires: ()
+;; Keywords: extensions
+
+;; eshell-buffer-capf.el is free software; you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; eshell-buffer-capf.el is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+;; General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Complete buffer expressions in Eshell.
+
 (defun eshell-buffer-capf--ppss ()
   "Parse the state of angle brackets at point."
   (let ((table (make-syntax-table)))
